b796db2e67839e580938a813d0c356c6b4c254d1
sebbyapudo <sebbyapudo@gmail.com>
2023-11-28 16:05:11 +0300

    Updated the page props

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import {Montserrat} from 'next/font/google'
+import Head from 'next/head'
 
 const montserrat = ({
   subsets: ["latin"],
@@ -8,9 +9,15 @@ const montserrat = ({
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={`${montserrat.variable} font-sans`}>
-      <Component {...pageProps} />
-    </main> 
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${montserrat.variable} font-mont bg-light w-full h-screen`}>
+        <Component {...pageProps} />
+      </main>
+    </> 
   )
   
 }
