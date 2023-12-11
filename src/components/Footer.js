4d05870e4c592b27d9536a07ba2994fc66cacad8
sebbyapudo <sebbyapudo@gmail.com>
2023-12-11 16:04:33 +0300

    Set up the footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Layout from './Layout'
+import Link from 'next/link'
+
+const Footer = () => {
+  return (
+    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg' >
+      <Layout className='py-8 flex items-center justify-between'>
+        <span>{new Date().getFullYear() } &copy; All rights reserved</span>
+        <div className='flex items-center'>
+          Built with  <span className='text-primary text-2xl px-1'>&#9825;</span> by&nbsp;
+          <Link href="https://github.com/sebbyapudo" target={"-blank"}
+          className='underline underline-offset-2'> 
+            <em>Sebby Apudo</em>
+          </Link>
+        </div>
+        
+        <Link href="https://www.linkedin.com/in/sebby-oluoch-48ba76218/"
+        target={"_blank"} className='underline underline-offset-2'
+        > <em>Say hello</em></Link>
+      </Layout>
+    </footer>
+  )
+}
+
+export default Footer
