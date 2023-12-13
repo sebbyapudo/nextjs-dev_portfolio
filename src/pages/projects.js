e0f183221a1f00117a3971ab083aaf7d59e57429
sebbyapudo <sebbyapudo@gmail.com>
2023-12-13 04:26:41 +0300

    Finished setting up the projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,13 +6,19 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import Felo from '../../public/images/projects/felo.jpg'
+import Recipe from '../../public/images/projects/recipe.jpg'
+import Kiongozi from '../../public/images/projects/kiongozi.jpg'
+import Logistics from '../../public/images/projects/logistics.jpg'
+import FoodStore from '../../public/images/projects/food-store.jpg'
+import Loruki from '../../public/images/projects/loruki.jpg'
 
 const FeaturedProject = ({type, title, summary, img, link, github}) => {
 
   return(
-    <article className='w-full flex items-center justify-between
+    <article className='w-full flex items-center justify-between relative rounded-br-2xl
     rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12
     '>
+      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl'/>
       <Link href={link} target='_blank'
       className='w-1/2 cursor-pointer overflow-hidden rounded-lg'
       >
@@ -37,6 +43,43 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
   )
 }
 
+const Project = ({title, type, img, link, github}) => {
+
+  return (
+    <article className='w-full flex flex-col items-center justify-center rounded-2xl
+    border border-solid border-dark bg-light p-6 relative
+    '>
+      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl'/>
+      <Link href={link} target='_blank'
+      className='w-full cursor-pointer overflow-hidden rounded-lg'
+      >
+        <Image src={img} alt={title} className='w-full h-auto' />
+      </Link>
+
+      <div className='w-full flex flex-col items-start justify-between mt-4'>
+        <span className='text-primary font-medium text-xl'>{type}</span>
+        <Link href={link} target='_blank' className='hover:underline underline-offset-2'>
+          <h2 className='my-2 w-full text-left text-3xl font-bold '>{title}</h2>
+        </Link>
+        
+
+        <div className='w-full mt-2 flex items-center justify-between'>
+          <Link href={link} target='_blank'
+            className=' text-lg font-semibold'
+          > <em className='underline underline-offset-2'> 
+            visit
+          </em>
+          </Link>
+          <Link href={github} target='_blank' className='w-8' > 
+            <GithubIcon />
+          </Link>
+    
+        </div>
+      </div>
+    </article>
+  )
+}
+
 const projects = () => {
   return (
     <>
@@ -48,7 +91,7 @@ const projects = () => {
         <Layout className='pt-16'>
           <AnimatedText text="Innovative Endeavors Showcase" className='mb-16' />
 
-          <div className='grid grid-cols-12 gap-24 '>
+          <div className='grid grid-cols-12 gap-24 gap-y-32'>
             <div className='col-span-12'>
               <FeaturedProject
                 title="Felix Omondi Photography"
@@ -61,23 +104,58 @@ const projects = () => {
             </div>
 
             <div className='col-span-6'>
-              Project 1
+              <Project
+                  title="Simply Recipes"
+                  link="https://sebbyapudo.github.io/SimplyRecipes/index.html"
+                  github="https://github.com/sebbyapudo/SimplyRecipes.git"
+                  type="Featured Project"
+                  img={Recipe}
+                />
             </div>
 
             <div className='col-span-6'>
-              project 2
+              <Project
+                    title="Skiongozi team building"
+                    link="https://sebbyapudo.github.io/SKiongozi_team-building/"
+                    github="https://github.com/sebbyapudo/SKiongozi_team-building.git"
+                    type="Featured Project"
+                    img={Kiongozi}
+
+              />
             </div>
 
             <div className='col-span-12'>
-              Featured project
+              <FeaturedProject
+                  title="Nathan Kayo Logistics"
+                  summary="The detailed organization and implementation of a complex operation. NKG a web app for a Logistics company in Nairobi, has multiple compatibilities on different screen devices as it fully responsive and it's built in plain old vanilla js "
+                  link="https://sebbyapudo.github.io/NKG-Logistics/"
+                  github="https://github.com/sebbyapudo/NKG-Logistics.git"
+                  type="Featured Project"
+                  img={Logistics}
+
+              />
             </div>
 
             <div className='col-span-6'>
-              Project 3
+              <Project
+                    title="Foodmart Ecommerce"
+                    link="https://www.figma.com/community/file/1271365468968017828/food-store-e-commerce"
+                    github="https://www.figma.com/file/gPWLVq0drcSpsuqoRTznRo/Food-Store-e-commerce?type=design&node-id=0%3A1&mode=design&t=N2k6Ss3H5nm9BsFD-1"
+                    type="Featured Project"
+                    img={FoodStore}
+
+                />
             </div>
 
             <div className='col-span-6'>
-              project 4
+              <Project
+                    title="Loruki CloudHosting"
+                    link="https://sebbyapudo.github.io/CloudHosting-Loruki/"
+                    github="https://github.com/sebbyapudo/CloudHosting-Loruki.git"
+                    type="Featured Project"
+                    img={Loruki}
+
+              />
             </div>
           </div>
         </Layout>
