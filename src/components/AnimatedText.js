b83e74b739c7ddfaad51ad8baf5e14a520cc5654
sebbyapudo <sebbyapudo@gmail.com>
2023-12-14 11:38:54 +0300

    Added dark mode on the animated text

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -31,7 +31,7 @@ const singleWord = {
 const AnimatedText = ({text, className=""}) => {
   return (
     <div className='w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden '>
-      <motion.h1 className={`inline-block w-full font-bold capitalize text-8xl ${className}`}
+      <motion.h1 className={`inline-block w-full font-bold capitalize text-8xl dark:text-light ${className}`}
       variants={quote}
       initial="initial"
       animate="animate"
