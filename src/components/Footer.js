ec6f1c34c4d120b4e511d10e263f6e779db4a8d4
sebbyapudo <sebbyapudo@gmail.com>
2023-12-15 12:45:38 +0300

    Made the footer component responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ import Link from 'next/link'
 const Footer = () => {
   return (
     <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light
-    dark:border-light
+    dark:border-light sm:text-base  
     ' >
-      <Layout className='py-8 flex items-center justify-between'>
+      <Layout className='py-8 flex items-center justify-between lg:flex-col lg:py-6'>
         <span>{new Date().getFullYear() } &copy; All rights reserved</span>
-        <div className='flex items-center'>
+        <div className='flex items-center lg:py-2'>
           Built with  <span className='text-primary dark:text-primaryDark text-2xl px-1'>&#9825;</span> by&nbsp;
           <Link href="https://github.com/sebbyapudo" target={"-blank"}
           className='underline underline-offset-2'> 
