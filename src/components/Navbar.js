a84dff39baa2fbacde347c5bca2db3bd62503df3
sebbyapudo <sebbyapudo@gmail.com>
2023-12-15 11:11:39 +0300

    Finished setting up the mobile navigation menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,13 +35,13 @@ const CustomMobileLink = ({href, title, className="", toggle}) => {
   }
 
   return (
-    <button href={href} className={`${className} relative group`} onClick={handleClick}>
+    <button href={href} className={`${className} relative group text-light dark:text-dark my-2`} onClick={handleClick}>
       {title}
 
       <span className={`
-      h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5
+      h-[1px] inline-block w-0 bg-light absolute left-0 -bottom-0.5
       group-hover:w-full transition-[width] ease duration-300
-      ${`router.asPath === ? 'w-full' : 'w-0' `} dark:bg-light
+      ${`router.asPath === ? 'w-full' : 'w-0' `} dark:bg-dark
       `}>
         &nbsp;
       </span>
@@ -133,47 +133,50 @@ const Navbar = () => {
 
       {
         isOpen ?
-        <div className='min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/75 dark:bg-light/75 rounded-lg backdrop-blur-md py-32'>
+        <motion.div
+        initial={{scale:0, opacity:0, x: "-50%", y: "-50%"}}
+        animate={{scale:1, opacity:1}}
+        className='min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/75 dark:bg-light/75 rounded-lg backdrop-blur-md py-32'>
         <nav className='flex flex-col items-center justify-center'>
-          <CustomMobileLink href="/" title="Home" className='mr-4' toggle={handleClick}/>
-          <CustomMobileLink href="/about" title="About" className='mx-4' toggle={handleClick}/>
-          <CustomMobileLink href="/projects" title="Projects" className='mx-4' toggle={handleClick}/>
-          <CustomMobileLink href="/articles" title="Articles" className='ml-4' toggle={handleClick}/>
+          <CustomMobileLink href="/" title="Home" className='' toggle={handleClick}/>
+          <CustomMobileLink href="/about" title="About" className='' toggle={handleClick}/>
+          <CustomMobileLink href="/projects" title="Projects" className='' toggle={handleClick}/>
+          <CustomMobileLink href="/articles" title="Articles" className='' toggle={handleClick}/>
         </nav>
 
-        <nav className='flex items-center justify-center flex-wrap'>
+        <nav className='flex items-center justify-center flex-wrap mt-2'>
           <motion.a href="https://twitter.com/ApudoSebby" target={'_blank'}
           whileHover={{y:-2}} 
           whileTap={{scale:0.9}}
-          className='w-6 mx-3'
+          className='w-6 mx-3 sm:mx-1' 
           >
             <TwitterIcon />
           </motion.a>
           <motion.a href="https://github.com/sebbyapudo" target={'_blank'}
           whileHover={{y:-2}}
           whileTap={{scale:0.9}}
-          className='w-6 mx-3'
+          className='w-6 mx-3 bg-light rounded-full dark:bg-dark sm:mx-1'
           >
             <GithubIcon />
           </motion.a>
           <motion.a href="https://www.linkedin.com/in/sebby-oluoch-48ba76218/" target={'_blank'}
           whileHover={{y:-2}}
           whileTap={{scale:0.9}}
-          className='w-6 mx-3'
+          className='w-6 mx-3 sm:mx-1'
           >
             <LinkedInIcon />
           </motion.a>
           <motion.a href="https://www.pinterest.com/sebbyapudo/" target={'_blank'}
           whileHover={{y:-2}}
           whileTap={{scale:0.9}}
-          className='w-6 mx-3 bg-light rounded-full'
+          className='w-6 mx-3 bg-light rounded-full sm:mx-1'
           >
             <PinterestIcon />
           </motion.a>
           <motion.a href="https://dribbble.com/JustSebby" target={'_blank'}
           whileHover={{y:-2}}
           whileTap={{scale:0.9}}
-          className='w-6 ml-3'
+          className='w-6 ml-3 sm:mx-1'
           >
             <DribbbleIcon />
           </motion.a>
@@ -192,7 +195,7 @@ const Navbar = () => {
           </button>
 
         </nav>
-        </div>
+        </motion.div>
 
         : null
       }
