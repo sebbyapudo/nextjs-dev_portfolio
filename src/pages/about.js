5ce1ef99f84ba61b85cef83cf913b06ecb0d7738
sebbyapudo <sebbyapudo@gmail.com>
2023-12-17 16:55:47 +0300

    Made the about page responsive

--- src/pages/about.js
+++ src/pages/about.js
@@ -45,9 +45,9 @@ const about = () => {
       </Head>
       <main className='w-full flex flex-col items-center justify-center dark:text-light'>
         <Layout className='pt-16'>
-          <AnimatedText text="Passion Ignites Purpose!" className='mb-16'/>
-          <div className='grid w-full grid-cols-8 gap-16'>
-            <div className='col-span-3 flex flex-col items-start justify-start'> 
+          <AnimatedText text="Passion Ignites Purpose!" className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8 '/>
+          <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
+            <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8'> 
               <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>About Me</h2>
               <p className='font-medium'>
                 Hi, I am <em>Sebby</em>  but you can just call me Seb. I am a web developer, 3D Artist and UI/UX designer. I craft aesthetically pleasing, intuitive digital experiences, driven by a three-year expertise. Eager to transform clients' ideas into compelling realities, I'm here to redefine your digital presence.
@@ -60,7 +60,7 @@ const about = () => {
               </p>
             </div>
             <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark
-            bg-light p-8 dark:bg-dark dark:border-light
+            bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8
             '>
               <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light'/>
               <Image src={AboutImage} alt='Sebby Apudo' className='w-full h-auto rounded-2xl'
@@ -70,26 +70,32 @@ const about = () => {
               33vw"
               />
             </div>
-            <div className='col-span-2 flex flex-col items-end justify-between'>
-              <div className='flex flex-col items-end justify-center'>
-                <span className='inliine-block text-7xl font-bold'>
+            <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3'>
+              <div className='flex flex-col items-end justify-center xl:items-center'>
+                <span className='inliine-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                   <AnimatedNumbers value={50}/> {'+'}
                 </span>
-                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>satisfied clients</h2>
+                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 
+                xl:text-center md:text-lg sm:text-base xs:text-sm
+                '>satisfied clients</h2>
               </div>
 
-              <div className='flex flex-col items-end justify-center'>
-                <span className='inliine-block text-7xl font-bold'>
+              <div className='flex flex-col items-end justify-center xl:items-center'>
+                <span className='inliine-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                   <AnimatedNumbers value={30}/> {'+'}
                 </span>
-                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>projects completed</h2>
+                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75
+                xl:text-center md:text-lg sm:text-base xs:text-sm
+                '>projects completed</h2>
               </div>
 
-              <div className='flex flex-col items-end justify-center'>
-                <span className='inliine-block text-7xl font-bold'>
+              <div className='flex flex-col items-end justify-center xl:items-center'>
+                <span className='inliine-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                   <AnimatedNumbers value={3}/> {'+'}
                 </span>
-                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>years of experience</h2>
+                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75
+                xl:text-center md:text-lg sm:text-base xs:text-sm
+                '>years of experience</h2>
               </div>
 
             </div>
