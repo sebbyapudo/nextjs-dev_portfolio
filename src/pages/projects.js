8fb36ebd9d97fa4143ce46e3be0eb1c25db0ea14
sebbyapudo <sebbyapudo@gmail.com>
2023-12-18 07:44:31 +0300

    Made the projects page responsive

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -17,25 +17,26 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
   return(
     <article className='w-full flex items-center justify-between relative rounded-br-2xl
     rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:bg-dark dark:border-light 
+    lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4
     '>
-      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark dark:bg-light rounded-br-3xl'/>
+      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark dark:bg-light rounded-br-3xl xs:-right-2 sm:h-[102%] xs:w-[100%] xs:rounded-[1.5rem]'/>
       <Link href={link} target='_blank'
-      className='w-1/2 cursor-pointer overflow-hidden rounded-lg'
+      className='w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full'
       >
         <Image src={img} alt={title} className='w-full h-auto' />
       </Link>
 
-      <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
-        <span className='text-primary dark:text-primaryDark font-medium text-xl'>{type}</span>
+      <div className='w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6'>
+        <span className='text-primary dark:text-primaryDark font-medium text-xl xs:text-base'>{type}</span>
         <Link href={link} target='_blank' className='hover:underline underline-offset-2'>
-          <h2 className='my-2 w-full text-left text-3xl font-bold dark:text-light'>{title}</h2>
+          <h2 className='my-2 w-full text-left text-3xl font-bold dark:text-light sm:text-sm'>{title}</h2>
         </Link>
-        <p className='my-2 font-medium text-dark dark:text-light'>{summary}</p>
+        <p className='my-2 font-medium text-dark dark:text-light sm:text-sm'>{summary}</p>
 
         <div className='mt-2 flex items-center'>
           <Link href={github} target='_blank' className='w-10' > <GithubIcon /></Link>
           <Link href={link} target='_blank'
-          className='ml-4 rounded-lg bg-dark dark:bg-light dark:text-dark text-light p-2 px-6 text-lg font-semibold'
+          className='ml-4 rounded-lg bg-dark dark:bg-light dark:text-dark text-light p-2 px-6 text-lg font-semibold sm:px-4 sm:text-base'
           > visit project</Link>
         </div>
       </div>
@@ -47,9 +48,9 @@ const Project = ({title, type, img, link, github}) => {
 
   return (
     <article className='w-full flex flex-col items-center justify-center rounded-2xl
-    border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light
+    border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light xs:p-4
     '>
-      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark dark:bg-light rounded-br-3xl'/>
+      <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark dark:bg-light rounded-br-3xl md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]'/>
       <Link href={link} target='_blank'
       className='w-full cursor-pointer overflow-hidden rounded-lg'
       >
@@ -57,20 +58,20 @@ const Project = ({title, type, img, link, github}) => {
       </Link>
 
       <div className='w-full flex flex-col items-start justify-between mt-4'>
-        <span className='text-primary dark:text-primaryDark font-medium text-xl'>{type}</span>
+        <span className='text-primary dark:text-primaryDark font-medium text-xl lg:text-lg md:text-base'>{type}</span>
         <Link href={link} target='_blank' className='hover:underline underline-offset-2'>
-          <h2 className='my-2 w-full text-left text-3xl font-bold dark:text-light'>{title}</h2>
+          <h2 className='my-2 w-full text-left text-3xl font-bold dark:text-light lg:text-2xl'>{title}</h2>
         </Link>
         
 
         <div className='w-full mt-2 flex items-center justify-between'>
           <Link href={link} target='_blank'
             className=' text-lg font-semibold'
-          > <em className='underline underline-offset-2'> 
+          > <em className='underline underline-offset-2 md:text-base'> 
             visit
           </em>
           </Link>
-          <Link href={github} target='_blank' className='w-8' > 
+          <Link href={github} target='_blank' className='w-8 md:w-6' > 
             <GithubIcon />
           </Link>
     
@@ -89,9 +90,9 @@ const projects = () => {
       </Head>
       <main className='w-full  flex flex-col items-center justify-center dark:text-light'>
         <Layout className='pt-16'>
-          <AnimatedText text="Innovative Endeavors Showcase" className='mb-16' />
+          <AnimatedText text="Innovative Endeavors Showcase" className='mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl' />
 
-          <div className='grid grid-cols-12 gap-24 gap-y-32'>
+          <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0'>
             <div className='col-span-12'>
               <FeaturedProject
                 title="Felix Omondi Photography"
@@ -103,7 +104,7 @@ const projects = () => {
               />
             </div>
 
-            <div className='col-span-6'>
+            <div className='col-span-6 sm:col-span-12'>
               <Project
                   title="Simply Recipes"
                   link="https://sebbyapudo.github.io/SimplyRecipes/index.html"
@@ -113,7 +114,7 @@ const projects = () => {
                 />
             </div>
 
-            <div className='col-span-6'>
+            <div className='col-span-6 sm:col-span-12'>
               <Project
                     title="Skiongozi team building"
                     link="https://sebbyapudo.github.io/SKiongozi_team-building/"
@@ -136,7 +137,7 @@ const projects = () => {
               />
             </div>
 
-            <div className='col-span-6'>
+            <div className='col-span-6 sm:col-span-12'>
               <Project
                     title="Foodmart Ecommerce"
                     link="https://www.figma.com/community/file/1271365468968017828/food-store-e-commerce"
@@ -147,7 +148,7 @@ const projects = () => {
                 />
             </div>
 
-            <div className='col-span-6'>
+            <div className='col-span-6 sm:col-span-12'>
               <Project
                     title="Loruki CloudHosting"
                     link="https://sebbyapudo.github.io/CloudHosting-Loruki/"
