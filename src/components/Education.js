74b6fbff649986d10eb78a0ba251f23cf689b9c3
sebbyapudo <sebbyapudo@gmail.com>
2023-12-18 06:54:51 +0300

    Made all the components in the about page responsive

--- src/components/Education.js
+++ src/components/Education.js
@@ -4,18 +4,18 @@ import LiIcon from './LiIcon'
 
 const Details = ({position, company, companyLink, time, work, address}) => {
   const ref = useRef(null);
-  return <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+  return <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]'>
     <LiIcon reference={ref} />
     <motion.div
     initial={{y:50}}
     whileInView={{y:0}}
     transition={{duration:0.5, type:"spring"}}
     >
-      <h3 className='capitalize font-bold text-2xl'>{position}&nbsp;<a href={companyLink} target='_blank' className='text-primary dark:text-primaryDark capitalize'>@{company}</a></h3>
-      <span className='capitalize font-medium text-dark/75 dark:text-light/75'>
+      <h3 className='capitalize font-bold text-2xl sm:text-xl xs:text-lg'>{position}&nbsp;<a href={companyLink} target='_blank' className='text-primary dark:text-primaryDark capitalize'>@{company}</a></h3>
+      <span className='capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm'>
         {time} | {address}
       </span>
-      <p className='font-medium w-full'>
+      <p className='font-medium w-full md:text-sm'>
         {work}
       </p>
     </motion.div>
@@ -32,13 +32,13 @@ const Education = () => {
   )
   return (
     <div className='my-64'>
-      <h2 className='w-full text-center text-8xl font-bold mb-32'>Education</h2>
+      <h2 className='w-full text-center text-8xl font-bold mb-32 md:text-6xl xs:text-4xl md:mb-16'>Education</h2>
 
-      <div ref={ref} className='w-[75%] mx-auto relative'>
+      <div ref={ref} className='w-[75%] mx-auto relative lg:w-[90%] md:w-full'>
         <motion.div
         style={{scaleY: scrollYProgress}}
-        className='absolute left-9 top-0 w-[4px] h-full bg-dark dark:bg-light origin-top'/>
-        <ul className='w-full flex flex-col items-center justify-between ml-4'>
+        className='absolute left-9 top-0 w-[4px] h-full bg-dark dark:bg-light origin-top md:w-[2px] md:left-[30px] xs:left-[20px]'/>
+        <ul className='w-full flex flex-col items-center justify-between ml-4 xs:ml-2'>
           <Details 
             position={"Bachelor Of Science In Software Engineering"} company={"KCA University"}
             companyLink={"https://www.kcau.ac.ke/"}
