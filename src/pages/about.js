6f3a0d7cb564c7aa314b29115f1004cc5f8e52e5
sebbyapudo <sebbyapudo@gmail.com>
2023-12-18 08:23:34 +0300

    Fixed a typo

--- src/pages/about.js
+++ src/pages/about.js
@@ -75,7 +75,7 @@ const about = () => {
             <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3'>
               <div className='flex flex-col items-end justify-center xl:items-center'>
                 <span className='inliine-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                  <AnimatedNumbers value={50}/> {'+'}
+                  <AnimatedNumbers value={50}/> {`+`}
                 </span>
                 <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 
                 xl:text-center md:text-lg sm:text-base xs:text-sm
@@ -84,7 +84,7 @@ const about = () => {
 
               <div className='flex flex-col items-end justify-center xl:items-center'>
                 <span className='inliine-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                  <AnimatedNumbers value={30}/> {'+'}
+                  <AnimatedNumbers value={30}/> {`+`}
                 </span>
                 <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75
                 xl:text-center md:text-lg sm:text-base xs:text-sm
@@ -93,7 +93,7 @@ const about = () => {
 
               <div className='flex flex-col items-end justify-center xl:items-center'>
                 <span className='inliine-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                  <AnimatedNumbers value={3}/> {'+'}
+                  <AnimatedNumbers value={3}/> {`+`}
                 </span>
                 <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75
                 xl:text-center md:text-lg sm:text-base xs:text-sm
