5cc059bdb47047f0c3bc35fffeccb7408fabcde9
sebbyapudo <sebbyapudo@gmail.com>
2024-03-18 10:24:44 +0300

    Added the design portfolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,12 +38,18 @@ export default function Home() {
                 Cheers : )            
               </p>
               <div className='flex items-center self-start mt-3 lg:self-center'>
-                <Link href="/Sebby-cv.pdf" target={"_blank"} download={true}
-                className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light
+                
+                <Link href="/Sebby-cv.pdf" target={"_blank"} 
+                className='flex  flex-grow-1 items-center bg-dark text-light p-2.5 px-6 mx-3 rounded-lg text-md font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light
                 hover:dark:border-light md:p-2 md:px-4 md:text-base
-                '
-                > Resume  <LinkArrow className={"w-6 ml-1"}/>
+                '> Resume <LinkArrow className={"w-5 ml-1"}/>
                 </Link>
+                <Link href="/Sebby-cv.pdf" target={"_blank"} 
+                className='flex  flex-grow-1 items-center bg-dark text-light p-2.5 px-6 rounded-lg text-md font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light
+                hover:dark:border-light md:p-2 md:px-4 md:text-base
+                '> Design portfolio  <LinkArrow className={"w-5 ml-1"}/>
+                </Link>
+
                 <Link href="mailto:sebbyapudo@gmail.com" target={"_blank"}
                 className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base'
                 ><em>Contact</em></Link>
